40062eeb4fea0624a40d0f3944723f8826a22510
fhw12 <nikita.pymitron@gmail.com>
2022-11-27 23:36:27 +0700

    rename draw function

--- src/main.c
+++ src/main.c
@@ -10,20 +10,20 @@ int main(){
 	lua_State *L = luaL_newstate();
 	luaL_loadfile(L, "main.lua");
 	luaL_openlibs(L);
-	lua_pcall(L, 0, 0, 0);
 
-	luaL_dostring(L, "love = { graphics = {} }");
+	luaL_dostring(L, "function draw() end\nlove = { graphics = {}, draw = draw  }\ndraw = nil");
 	lua_pushcfunction(L, graphics_rectangle);
 	lua_setglobal(L, "graphics_rectangle");
 	luaL_dostring(L, "love.graphics.rectangle = graphics_rectangle");
+
+	lua_pcall(L, 0, 0, 0);
 	
 	InitWindow(800, 600, "l2d");
 
 	while(!WindowShouldClose()){
 		BeginDrawing();
 			ClearBackground((Color){0, 0, 0});
-			lua_getglobal(L, "update");
-			lua_pcall(L, 0, 0, 0);
+			luaL_dostring(L, "love.draw()");
 		EndDrawing();
 	}
 
--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,4 @@
-function update()
+function love.draw()
 	for x = 0, 10 do
 		for y = 0, 10 do
 			love.graphics.rectangle(100+x*30, 100+y*30, 20, 20)
